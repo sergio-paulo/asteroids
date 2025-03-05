460e928b3d4d565f9c9d66d2ffd19fc0b832ea28
sergio.paulo <sergio.e.r.c.paulo@gmail.com>
2025-03-05 01:08:55 +0000

    G: added player.py and Player class

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         time = clock.tick(60)
         dt = time / 1000
